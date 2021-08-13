28a0e73785fb67b911296ae479593f635c888a88
alxbaylon <alxbay97@gmail.com>
2021-08-13 03:27:16 -0500

    JS logic ready

--- index.js
+++ index.js
@@ -1,3 +1,55 @@
-const onPricesResult = () =>{
-	alert("holis");
+//EXcepción: tecla enter en el input de cantidad de películas.
+const onPricesResult = () => {
+	const inputMovies = document.getElementById('InputMoviesNumber');
+	const moviesValues = inputMovies.value;
+
+	if(moviesValues){
+		const moviesTotalPrice = moviesValues * theaterPrice;
+
+		let precioStreaming = 0;
+		streamings.forEach((streaming) => {
+			if(streaming.state){
+				precioStreaming += streaming.price;
+			}
+		});
+
+		let winner;
+		if(precioStreaming > moviesTotalPrice){
+			winner = 'paga mejor el cine';
+		} else if(precioStreaming < moviesTotalPrice){
+			winner = 'paga mejor el streaming';
+		} else {
+			winner = 'cuestan igual, paga la que te convenga';
+		}
+
+		const result = document.getElementById('ResultP');
+		result.innerText = `El costo de las peliculas en cine es: $${moviesTotalPrice} MXN
+		El costo de las peliculas en streaming es: $${precioStreaming} MXN
+		Por lo anterior ${winner}`
+	} else {
+		alert('Escoge bien por favor');
+		inputMovies.focus();
+	}
 }
+//-----Checkbox Logic-----------------------------------
+const netflixCheck = () => {
+	let n = 0;
+	confirmStreaming(n);
+}
+const hboMaxCheck = () =>{
+	let n = 1;
+	confirmStreaming(n);
+}
+const confirmStreaming = (n) => {
+	if (streamings[n].state == false){
+		streamings[n].state = true;
+	} else if (streamings[n].state == true){
+		streamings[n].state = false;
+	} 
+}
+//-----Prices------------------------------------------
+const theaterPrice = 70;
+const streamings = [
+	{name: 'Netflix', price:139, state: false},
+	{name: 'HBO-Max', price:99, state: false},
+];
